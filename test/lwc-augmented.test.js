d7d2c7dcb7aaa4f753008ade2621a4960a7584ed
Julien Roche <julien.roche@salesforce.com>
2022-09-09 14:01:21 +0200

    test: first test

--- test/lwc-augmented.test.js
+++ test/lwc-augmented.test.js
@@ -1,55 +1,23 @@
-import path from 'path';
-import * as url from 'url';
 import eslint from 'eslint';
-import * as eslintrc from '@eslint/eslintrc';
-import { jest } from '@jest/globals';
+import * as eslintParser from '@babel/eslint-parser';
 
-import eslintLwcAugmented from '../index.js';
+const { RuleTester } = eslint;
 
-const { ESLint } = eslint;
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const baseTestFiles = path.resolve(path.join(__dirname, './fake-files/'));
-const mockEslintLwcAugmentedPath = path.resolve(__dirname, '../index.js');
-
-jest.mock(
-  '@eslint/eslintrc',
-  () => {
-    return {
-      ModuleResolver: {
-        ...jest.requireActual('@eslint/eslintrc'),
-        resolve: () => {
-          return mockEslintLwcAugmentedPath;
+describe('eslint-plugin-lwc-augmented', () => {
+  test('could instantiate ruleTester', () => {
+    const ruleTester = new RuleTester({
+      parser: eslintParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          parserOpts: {
+            plugins: ['classProperties', ['decorators', { decoratorsBeforeExport: false }]]
+          }
         }
-      }
-    };
-  },
-  { virtual: true }
-);
-
-describe('eslint-plugin-lwc-augmented - ', () => {
-  let instance;
-
-  beforeEach(() => {
-    jest.resetModules();
-    jest.spyOn(eslintrc.ModuleResolver, 'resolve').mockImplementation(() => mockEslintLwcAugmentedPath);
-  });
-
-  beforeEach(async () => {
-    instance = new ESLint({
-      baseConfig: eslintLwcAugmented.configs.recommended,
-      useEslintrc: false,
-      ignore: false
+      },
+      extends: ['eslint:recommended']
     });
-  });
-
-  function extractErrorReport(errorReport) {
-    return errorReport.messages.map(({ ruleId, message }) => `${ruleId}::${message}`);
-  }
-
-  test('with basic code', async () => {
-    const eslintReport = await instance.lintFiles([`${baseTestFiles}/basic.js`]);
-    const [errorReport] = eslintReport.results;
-
-    expect(extractErrorReport(errorReport)).toMatchSnapshot();
+    
+    expect(ruleTester).toBeDefined();
   });
 });
